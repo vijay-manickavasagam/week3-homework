0b50de415027f762c2c2290d00ab302f697731c0
Jeff Cohen <jeff@starterleague.com>
2014-01-23 10:33:06 -0600

    Homework for Week 3

---
+++ monopoly.rb
@@ -0,0 +1,36 @@
+# Getting Practice with Hashes and Arrays
+
+cheap_property = { :name => 'baltic', :price => 60, :rent => 2 }
+rich_property = { :name => 'boardwalk', :price => 400, :rent => 50 }
+more_properties = [ { :name => 'Virginia Ave', :price => 60, :rent => 10},
+                    { :name => 'Marvin Gardens', :price => 160, :rent => 20},
+                    { :name => 'New York Ave', :price => 180, :rent => 18},
+                   ]
+
+special_squares = { :good => { :go => "Go", :car => "Free Parking", :nojail => "Just Visiting" },
+                    :bad => { :jail => "Go To Jail", :pay => "Luxury Tax" }
+                  }
+
+# Q1. How much does it cost to buy Baltic?
+# A1: Here's the answer:
+puts cheap_property[:price]
+
+#Q2. How much does it cost to buy Boardwalk?
+#A2: Your code goes here:
+
+
+#Q3. How much is rent for Baltic?
+#A3: Your code goes here:
+
+
+#Q4. How much does it cost to buy New York Ave?
+#A4: Your code goes here:
+
+
+#Q5: How many "good" special squares are there?
+#A6: Your code goes here:
+
+
+#Q7: What is printed on the gameboard in the special "car" square?
+#A7: Your code goes here:
+
