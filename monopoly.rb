551573865c56942e7e31af6a491ed3d2aa53e308
Jeff Cohen <jeff@starterleague.com>
2014-01-23 10:41:22 -0600

    Update homework

--- monopoly.rb
+++ monopoly.rb
@@ -7,30 +7,32 @@ more_properties = [ { :name => 'Virginia Ave', :price => 60, :rent => 10},
                     { :name => 'New York Ave', :price => 180, :rent => 18},
                    ]
 
-special_squares = { :good => { :go => "Go", :car => "Free Parking", :nojail => "Just Visiting" },
-                    :bad => { :jail => "Go To Jail", :pay => "Luxury Tax" }
+special_squares = { :good => ["Go", "Free Parking", "Just Visiting"],
+                    :bad => ["Go To Jail", "Luxury Tax"]
                   }
 
-# Q1. How much does it cost to buy Baltic?
-# A1: Here's the answer:
+#Q1. How much does it cost to buy Baltic?
+#A1: Here's the answer:
 puts cheap_property[:price]
 
 #Q2. How much does it cost to buy Boardwalk?
 #A2: Your code goes here:
 
 
+
 #Q3. How much is rent for Baltic?
 #A3: Your code goes here:
+
 
 
 #Q4. How much does it cost to buy New York Ave?
 #A4: Your code goes here:
 
 
+
 #Q5: How many "good" special squares are there?
-#A6: Your code goes here:
+#A5: Your code goes here:
 
 
-#Q7: What is printed on the gameboard in the special "car" square?
-#A7: Your code goes here:
+
 
